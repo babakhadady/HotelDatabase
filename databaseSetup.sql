b4a00184ba8688bc55183df98e3faebd0af985a2
Henry Kim <henrykim966@gmail.com>
2023-04-02 19:29:45 -0700

    added more instance for script and tested queries

--- databaseSetup.sql
+++ databaseSetup.sql
@@ -29,12 +29,34 @@ create table roomContains
  
 grant ALL PRIVILEGES on roomContains to public;
 
+/*
+roomContains 
+*/
+insert into roomContains values(200, 2, 'single', 'vacant', 150);
+insert into roomContains values(201, 2, 'single', 'vacant', 150);
+insert into roomContains values(202, 2, 'double', 'vacant', 200);
 
-insert into roomContains values
-(200, 2, 'single', 'vacant', 150);
+insert into roomContains values(300, 3, 'single', 'occupied', 150);
+insert into roomContains values(301, 3, 'double', 'vacant', 200);
+insert into roomContains values(302, 3, 'queen', 'vacant', 250);
 
-insert into roomContains values
-(201, 2, 'single', 'vacant', 150);
+insert into roomContains values(400, 4, 'single', 'occupied', 150);
+insert into roomContains values(401, 4, 'double', 'occupied', 200);
+insert into roomContains values(402, 4, 'king', 'vacant', 300);
 
-insert into roomContains values
-(202, 2, 'double', 'vacant', 200);
+insert into roomContains values(500, 5, 'single', 'occupied', 150);
+insert into roomContains values(501, 5, 'double', 'vacant', 200);
+insert into roomContains values(502, 5, 'master', 'occupied', 350);
+
+/*
+reservations 
+*/
+insert into reservations values('jan 10', 'jan 12', 1);
+insert into reservations values('jan 10', 'jan 12', 2);
+
+/*
+reservations 
+*/
+insert into reserves values(2, 300);
+insert into reserves values(2, 301);
+insert into reserves values(1, 302);
